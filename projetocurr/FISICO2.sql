73844855616172ebdb4b901c545b4696da083559
projeto-felipe-kenzo <79851474+projeto-felipe-kenzo@users.noreply.github.com>
2022-08-01 09:08:21 -0300

    Add files via upload

---
+++ projetocurr/FISICO2.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE projeto;
+USE projeto;
+
+
+CREATE TABLE usuarios (
+id int PRIMARY KEY AUTO_INCREMENT,
+nome varchar(255),
+email varchar(255),
+senha varchar(255),
+cpf varchar(14),
+tipo int
+);
+
+CREATE TABLE curriculo (
+id_curr int PRIMARY KEY AUTO_INCREMENT,
+nome varchar(255),
+email varchar(255),
+telefone varchar(14),
+curso varchar(255)
+);
+
+CREATE TABLE competencias (
+id_comp int PRIMARY KEY AUTO_INCREMENT,
+competencia varchar(255),
+id_curr int,
+FOREIGN KEY(id_curr) REFERENCES curriculo (id_curr)
+);
+
+CREATE TABLE habilidades (
+id_hab int PRIMARY KEY AUTO_INCREMENT,
+habilidade varchar(255),
+id_curr int,
+FOREIGN KEY(id_curr) REFERENCES curriculo (id_curr)
+);
+
+CREATE TABLE educacao (
+id_edu int PRIMARY KEY AUTO_INCREMENT,
+instituicao varchar(255),
+curso varchar(255),
+inicio date,
+fim date,
+id_curr int,
+FOREIGN KEY(id_curr) REFERENCES curriculo (id_curr)
+);
+
+CREATE TABLE experiencia (
+id_exp int PRIMARY KEY AUTO_INCREMENT,
+ocupacao varchar(255),
+empresa varchar(255),
+inicio date,
+fim date,
+id_curr int,
+FOREIGN KEY(id_curr) REFERENCES curriculo (id_curr)
+);
+
